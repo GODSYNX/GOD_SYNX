266805bd64f3982e2501bdd20fa92c174b3b7285
GODSYNX <86106517+GODSYNX@users.noreply.github.com>
2021-09-26 13:46:48 +0700

    Create SCRIPTV1.lua

---
+++ SCRIPTV1.lua
@@ -0,0 +1,138 @@
+wait(1)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Cr.patsa123456";
+Text = "";
+Duration = 2;
+})
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
+ 
+local window = library:CreateWindow("God HubV1") -- Creates the window
+ 
+local farm = window:CreateFolder(" Build a Boat Hack") -- Creates the folder(U will put here your buttons,etc)
+farm:Button("Farm Loop",function()
+--noclip
+noclip = true
+game:GetService('RunService').Stepped:connect(function()
+if noclip then
+game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+end
+end)
+--Loop
+for i=1, math.huge do
+local waittime = 2
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-57.5911636, 54.7921333, 1370.4563, -0.999964356, 2.47187559e-09, 0.00849207584, 1.31207345e-09, 1, -1.36576602e-07, -0.00849207584, -1.36560487e-07, -0.999964356)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-53.8646736, 54.7921333, 2135.83691, -0.999980807, 2.17031482e-09, 0.00628393609, 1.31207345e-09, 1, -1.36576602e-07, -0.00628393609, -1.36565603e-07, -0.999980807)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-54.3710403, 54.7921333, 2905.50195, -0.999980986, 4.50923493e-10, -0.00630529039, 1.31207345e-09, 1, -1.36576602e-07, 0.00630529039, -1.36582273e-07, -0.999980986)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-69.8425522, 54.7921333, 3679.34131, -0.999970496, 2.40644699e-10, -0.00784481037, 1.31207345e-09, 1, -1.36576602e-07, 0.00784481037, -1.36582784e-07, -0.999970496)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-65.0485001, 54.7921333, 4447.39844, -0.999684751, -2.12410178e-09, -0.0251565129, 1.31207345e-09, 1, -1.36576602e-07, 0.0251565129, -1.36566513e-07, -0.999684751)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-71.7007523, 54.7921333, 5216.17676, -0.999772847, -1.60741076e-09, -0.0213741828, 1.31207345e-09, 1, -1.36576602e-07, 0.0213741828, -1.36573618e-07, -0.999772847)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-65.1917725, 54.7921333, 5984.92236, -0.999943554, 2.77545831e-09, 0.0107153719, 1.31207345e-09, 1, -1.36576602e-07, -0.0107153719, -1.36554789e-07, -0.999943554)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-63.3787956, 54.7921333, 6762.92627, -0.999820352, -1.27981348e-09, -0.0189757496, 1.31207345e-09, 1, -1.36576602e-07, 0.0189757496, -1.36576858e-07, -0.999820352)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-71.7578735, 54.7921333, 7524.47363, -0.999998212, 1.07228848e-09, -0.00175556378, 1.31207345e-09, 1, -1.36576602e-07, 0.00175556378, -1.36578578e-07, -0.999998212)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-76.3128815, 54.7921333, 8295.3916, -0.999999642, 1.30899847e-09, -2.23945826e-05, 1.31207345e-09, 1, -1.36576602e-07, 2.23945826e-05, -1.36576517e-07, -0.999999642)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-55.9044495, -359.506073, 9496.25586, -0.999835372, 4.94488894e-08, -0.0181456991, 4.9011863e-08, 1, 2.45287808e-08, 0.0181456991, 2.36353888e-08, -0.999835372)
+wait(waittime)
+wait(20)
+end    
+end)
+farm:Button("Farm OneTime",function()
+--noclip
+noclip = true
+game:GetService('RunService').Stepped:connect(function()
+if noclip then
+game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+end
+end)    
+--one time
+local waittime = 2
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-57.5911636, 54.7921333, 1370.4563, -0.999964356, 2.47187559e-09, 0.00849207584, 1.31207345e-09, 1, -1.36576602e-07, -0.00849207584, -1.36560487e-07, -0.999964356)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-53.8646736, 54.7921333, 2135.83691, -0.999980807, 2.17031482e-09, 0.00628393609, 1.31207345e-09, 1, -1.36576602e-07, -0.00628393609, -1.36565603e-07, -0.999980807)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-54.3710403, 54.7921333, 2905.50195, -0.999980986, 4.50923493e-10, -0.00630529039, 1.31207345e-09, 1, -1.36576602e-07, 0.00630529039, -1.36582273e-07, -0.999980986)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-69.8425522, 54.7921333, 3679.34131, -0.999970496, 2.40644699e-10, -0.00784481037, 1.31207345e-09, 1, -1.36576602e-07, 0.00784481037, -1.36582784e-07, -0.999970496)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-65.0485001, 54.7921333, 4447.39844, -0.999684751, -2.12410178e-09, -0.0251565129, 1.31207345e-09, 1, -1.36576602e-07, 0.0251565129, -1.36566513e-07, -0.999684751)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-71.7007523, 54.7921333, 5216.17676, -0.999772847, -1.60741076e-09, -0.0213741828, 1.31207345e-09, 1, -1.36576602e-07, 0.0213741828, -1.36573618e-07, -0.999772847)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-65.1917725, 54.7921333, 5984.92236, -0.999943554, 2.77545831e-09, 0.0107153719, 1.31207345e-09, 1, -1.36576602e-07, -0.0107153719, -1.36554789e-07, -0.999943554)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-63.3787956, 54.7921333, 6762.92627, -0.999820352, -1.27981348e-09, -0.0189757496, 1.31207345e-09, 1, -1.36576602e-07, 0.0189757496, -1.36576858e-07, -0.999820352)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-71.7578735, 54.7921333, 7524.47363, -0.999998212, 1.07228848e-09, -0.00175556378, 1.31207345e-09, 1, -1.36576602e-07, 0.00175556378, -1.36578578e-07, -0.999998212)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-76.3128815, 54.7921333, 8295.3916, -0.999999642, 1.30899847e-09, -2.23945826e-05, 1.31207345e-09, 1, -1.36576602e-07, 2.23945826e-05, -1.36576517e-07, -0.999999642)
+wait(waittime)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-55.9044495, -359.506073, 9496.25586, -0.999835372, 4.94488894e-08, -0.0181456991, 4.9011863e-08, 1, 2.45287808e-08, 0.0181456991, 2.36353888e-08, -0.999835372)
+wait(waittime)
+wait(20)
+end)
+farm:Button("Fix noclip",function()
+--Fix noclip
+noclip = false
+game:GetService('RunService').Stepped:connect(function()
+if noclip then
+game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+end
+end)
+end)
+--tp team
+local tp = window:CreateFolder("Tp team color")
+tp:Button("Black",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-336.34494, -9.90199184, -77.173317, 0.00310703064, -9.93006282e-08, -0.999995172, 3.54395802e-09, 1, -9.92900979e-08, 0.999995172, -3.23544347e-09, 0.00310703064)
+end)
+tp:Button("Blue",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(220.001968, -9.90199184, 294.825562, 0.00205284334, -6.90170623e-08, 0.999997914, 1.29365567e-08, 1, 6.89906514e-08, -0.999997914, 1.27949029e-08, 0.00205284334)
+end)
+tp:Button("Green",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-354.530548, -9.90198994, 283.118652, -0.0184541512, -9.24654984e-08, -0.99982971, -2.84550961e-08, 1, -9.19560463e-08, 0.99982971, 2.67532787e-08, -0.0184541512)
+end)
+tp:Button("Magenta",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(230.023071, -9.90198994, 651.762329, -0.0293001886, -1.30176145e-08, 0.999570668, 8.36125214e-09, 1, 1.32682976e-08, -0.999570668, 8.74642581e-09, -0.0293001886)
+end)
+tp:Button("Red",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(222.717834, -9.90199184, -67.8631592, -0.0125743672, -3.33527197e-08, 0.999920964, 6.39347064e-08, 1, 3.41593562e-08, -0.999920964, 6.43591846e-08, -0.0125743672)
+end)
+tp:Button("White",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-49.2847977, -9.90199184, -358.887482, -0.999506235, -1.96958911e-08, -0.0314216614, -2.00378256e-08, 1, 1.05672031e-08, 0.0314216614, 1.11916068e-08, -0.999506235)
+end)
+tp:Button("Yellow",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-314.146423, -9.90199089, 645.362488, 0.0125740599, -3.61553347e-08, -0.999920964, -7.422301e-09, 1, -3.6251528e-08, 0.999920964, 7.87754306e-09, 0.0125740599)
+end)
+--Admin
+local Ad = window:CreateFolder("Admin Hub")
+Ad:Button("ShatterVast Admin",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/iL4NRDux"))()
+end)
+Ad:Button("Infinite Yield",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/05i9QcJa"))()
+end)
+Ad:Button("CMD X",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/6wLsMGQN"))()
+end)
+Ad:Button("Fates Admin",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/jE5xCC7w"))()
+end)
+Ad:Button("Reviz Admin",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/fAkyWMeT"))()
+end)
+--Credit
+local Credit = window:CreateFolder("Credit")
+Credit:Label("patsa123456",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(53,168,18); -- text
+    BgColor = Color3.fromRGB(0,0,0);--back gro
+    
+}) 
