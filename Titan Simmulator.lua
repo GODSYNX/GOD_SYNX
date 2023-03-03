19f510d36ec9efafd5dd30efd7aa7122742c1d02
GODSYNX <86106517+GODSYNX@users.noreply.github.com>
2023-03-03 23:32:50 +0700

    Update Titan Simmulator.lua

--- Titan Simmulator.lua
+++ Titan Simmulator.lua
@@ -68,7 +68,7 @@ game:GetService("RunService").RenderStepped:Connect(function()
 for i,v in pairs(game.Workspace:GetChildren()) do
 if v.Name == "Drop" then
 v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-v.CanCollide = false
+v.CanCollide = true
 end
 end
 end)
