1be0f1ed1e491dcef11718628971cb2f82f95a68
GODSYNX <86106517+GODSYNX@users.noreply.github.com>
2023-03-09 23:11:25 +0700

    Update Titan Simmulator.lua

--- Titan Simmulator.lua
+++ Titan Simmulator.lua
@@ -1,7 +1,8 @@
 --[[
-_G.Key = "" Buy Key for Use or Dm Discord Killer Fish Form Sandiego#8589
-_G.AutoConfig = 1 -- if You Use Normal Exploit use 1 or you use Synapse X use 2
-_G.Script_Mode = 1 --1 is Titan Simulator 2 is ...
+_G.Key = "" -- Buy Key for Use or Dm Discord Killer Fish Form Sandiego#8589
+_G.AutoConfigEnb = false -- enabled = true disbled = false
+_G.AutoConfig = 1 -- if You Use Normal Exploit type 1 or you use Synapse X Type 2
+_G.Script_Mode = 1 --1 is Titan Simulator 2 is Coming Soon
 _G.HookFunction = false -- Anti Kick Work 100% Errol 267 
 _G.SynapseX = false -- if you Use SynapseX kick0% warning if you don't use SynapseX kick30%
 _G.Webhook = false -- warning so UltraFast ping to discord and Break100% your Exploit if you not SynapseX
@@ -9,6 +10,24 @@ _G.Webhook_URL = ""--Your WebHook
 --]]
 repeat wait() until game:IsLoaded()
 repeat wait() local a, b = pcall(function()
+            -----------------------------
+     if _G.AutoConfigEnb == true then
+if _G.AutoConfig == 1 then
+_G.Script_Mode = 1 --1 is Titan Simulator 2 is ...
+_G.HookFunction = false -- Anti Kick Work 100% Error 267 
+_G.SynapseX = false -- if you Use SynapseX kick0% warning if you don't use SynapseX kick30%
+_G.Webhook = false -- warning so UltraFast ping to discord and Break100% your Exploit if you not SynapseX
+_G.Webhook_URL = ""--Your WebHook
+end
+if _G.AutoConfig == 2 then
+_G.Script_Mode = 1 --1 is Titan Simulator 2 is ...
+_G.HookFunction = true -- Anti Kick Work 100% Error 267 
+_G.SynapseX = true -- if you Use SynapseX kick0% warning if you don't use SynapseX kick30%
+_G.Webhook = false -- warning so UltraFast ping to discord and Break100% your Exploit if you not SynapseX
+_G.Webhook_URL = ""--Your WebHook
+end
+    end   
+          ---------------------------------------------
 if _G.Script_Mode == 1 then
 -------------------------------------------------------------
 if _G.HookFunction then
