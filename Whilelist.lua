f719af73a8a790d911a56343601b2de21a63ff20
GODSYNX <86106517+GODSYNX@users.noreply.github.com>
2023-03-09 23:06:58 +0700

    Update Whilelist.lua

--- Whilelist.lua
+++ Whilelist.lua
@@ -2,7 +2,7 @@
 _G.Key = "" -- Buy Key for Use or Dm Discord Killer Fish Form Sandiego#8589
 _G.AutoConfigEnb = false -- enabled = true disbled = false
 _G.AutoConfig = 1 -- if You Use Normal Exploit use 1 or you use Synapse X use 2
-_G.Script_Mode = 1 --1 is Titan Simulator 2 is ...
+_G.Script_Mode = 1 --1 is Titan Simulator 2 is Coming Soon
 _G.HookFunction = false -- Anti Kick Work 100% Errol 267 
 _G.SynapseX = false -- if you Use SynapseX kick0% warning if you don't use SynapseX kick30%
 _G.Webhook = false -- warning so UltraFast ping to discord and Break100% your Exploit if you not SynapseX
