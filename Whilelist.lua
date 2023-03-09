b60a6106c17deec6fe3689036ac21b8065bd1eca
GODSYNX <86106517+GODSYNX@users.noreply.github.com>
2023-03-09 22:44:05 +0700

    Update Whilelist.lua

--- Whilelist.lua
+++ Whilelist.lua
@@ -1,5 +1,5 @@
 --[[
-_G.Key = ""
+_G.Key = "" Buy Key for Use or Dm Discord Killer Fish Form Sandiego#8589
 _G.Script_Mode = 1 --1 is Titan Simulator 2 is ...
 _G.HookFunction = false -- Anti Kick Work 100% Error 267 
 _G.SynapseX = false -- if you Use SynapseX kick0% warning if you don't use SynapseX kick30%
@@ -10,12 +10,16 @@ local KeyCkecked = false
 local Hwid = nil
 local HwidExec = game:GetService("RbxAnalyticsService"):GetClientId()
 
-if _G.Key == "FPCQTWZJRVMEMKFF" then
-    Hwid = "F784E3B4-73E0-4B11-B658-72D453A8F0EE"
+if _G.Key == "FPCQTWZJRVMEMKFF" then 
+    Hwid = "F784E3B4-73E0-4B11-B658-72D453A8F0EE" -- My Hwid Synapse X
     KeyCkecked = true
 end
 if _G.Key == "UCFVYGECAFEPUYMZ" then
-    Hwid = ""
+    Hwid = "" --Hwid My JJsploit
+    KeyCkecked = true
+end
+if _G.Key == "TRIGONNOOB" then
+    Hwid = "" -- My Hwid TRIGON
     KeyCkecked = true
 end
 if KeyCkecked == true then
